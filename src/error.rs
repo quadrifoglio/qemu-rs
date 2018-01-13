c295d542629615ef0eb75d77b945b472293f21a3
quadrifoglio <quadrifoglio.clement@protonmail.com>
2018-01-13 13:34:09 +0100

    using failure for error handling

---
+++ src/error.rs
@@ -0,0 +1,13 @@
+//! Error handling functionality.
+
+use failure;
+
+pub type Result<T> = ::std::result::Result<T, failure::Error>;
+
+#[derive(Debug, Fail)]
+pub enum InitError {
+    #[fail(display = "could not find QEMU executable: {}", exec)]
+    ExecutableNotFound {
+        exec: String,
+    }
+}
